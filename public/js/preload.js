9f9dd83da3296b950a8dc1c09c394fb20b6f1ecc
Rob Muhlestein <rob@skilstak.com>
2015-10-31 13:45:37 -0400

    save

--- public/js/preload.js
+++ public/js/preload.js
@@ -11,6 +11,10 @@ Flappy.Preload.prototype = {
     this.logo.anchor.set(0.5);
     this.logo.animations.add('blink');
     this.logo.play('blink',2,true);
+    
+    var style = { font: "30px sans-serif", fill: "#fdf6e3", align: "center" }; 
+    this.loading = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 70, "Loading...", style);
+    this.loading.anchor.set(0.5,0.5);
 
     this.load.image('background','assets/background.png');
     this.load.spritesheet('flappy','assets/flappy.png',46,64);
@@ -26,10 +30,10 @@ Flappy.Preload.prototype = {
     this.load.audio('crash', 'assets/crash.wav');
     this.load.audio('play', 'assets/play.mp3');
     this.load.audio('gameover', 'assets/gameover.wav');
-
   },
 
   create: function () {
+    this.loading.text = "Decoding...";
     var start = this.game.add.audio('start');
     var flap = this.game.add.audio('flap');
     var play = this.game.add.audio('play',0.7,true);
